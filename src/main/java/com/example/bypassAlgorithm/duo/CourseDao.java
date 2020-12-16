739a58ff632bd904b8dc34812289e2e846e1e72b
Gurpreet Singh <gurpreetsingh.qualhon@gmail.com>
2020-12-16 23:04:47 +0530

    remove unwanted files

--- src/main/java/com/example/bypassAlgorithm/duo/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.bypassAlgorithm.duo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bypassAlgorithm.entity.Courses;
-
-public interface CourseDao extends JpaRepository<Courses, Long>{
-
-}
